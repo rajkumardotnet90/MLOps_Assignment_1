834233438197c378a953514abd21156f357402fa
rajkumardotnet90 <rajkumardotnet90@gmail.com>
2025-10-05 19:30:23 +0530

    unwanted folder removal

--- boston-housing-ml-1/src/train_kernel_ridge.py
+++
@@ -1,7 +0,0 @@
-def train_kernel_ridge(X_train, y_train, alpha=1.0, kernel='linear'):
-    from sklearn.kernel_ridge import KernelRidge
-    
-    model = KernelRidge(alpha=alpha, kernel=kernel)
-    model.fit(X_train, y_train)
-    
-    return model
